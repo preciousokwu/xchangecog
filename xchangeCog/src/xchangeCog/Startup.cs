7e34f6e7771a3a17379730cd858a6801892951d0
Femi Ojo <femiojo24@gmail.com>
2017-03-29 04:13:56 +0100

    Test Commit

--- xchangeCog/src/xchangeCog/Startup.cs
+++ xchangeCog/src/xchangeCog/Startup.cs
@@ -12,7 +12,7 @@ namespace xchangeCog
 {
     public class Startup
     {
-        //
+        //Fojo test commit
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
